7309905a5b779f89a390cd03ad5d5cbad68fcf95
LabLamb <lablambworks@gmail.com>
2020-06-28 22:28:27 +0800

    Add socket

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const schedule = require('node-schedule'),
     app = require('express')(),
     http = require('http').createServer(app),
-    socket = require('socket.io')
+    io = require('socket.io')
 
 app.get(`/`, (req, res) => {
     res.send(`Please visit: <br><a href="https://github.com/LabLamb/MTR-Cards-Backend">https://github.com/LabLamb/MTR-Cards-Backend</a><br> for more info.`)
@@ -11,4 +11,5 @@ http.listen(process.env.PORT || 3000)
 
 var job = schedule.scheduleJob('*/10 * * * * *', () => {
     console.log(`This job should be running every 10 seconds.`);
+    io.of('/').emit(`This job should be running every 10 seconds.`)
 });
