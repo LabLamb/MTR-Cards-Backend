e6faebcc57e55da9d2e11e9b08285275f956c756
LabLamb <lablambworks@gmail.com>
2020-06-28 22:25:26 +0800

    Make dummy page

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const schedule = require('node-schedule')
+const schedule = require('node-schedule'),
+    app = require('express')(),
+    http = require('http').createServer(app),
+    socket = require('socket.io')
+
+app.get(`/`, (req, res) => {
+    res.send(`Please visit: <br><a href="https://github.com/LabLamb/MTR-Cards-Backend">https://github.com/LabLamb/MTR-Cards-Backend</a><br> for more info.`)
+})
+
+http.listen(process.env.PORT || 3000)
 
 var job = schedule.scheduleJob('*/10 * * * * *', () => {
-    console.log('The answer to life, the universe, and everything!');
+    console.log(`This job should be running every 10 seconds.`);
 });
-
-// job.invoke()
