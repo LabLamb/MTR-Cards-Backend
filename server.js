909f2ea084ba149338d1a09ce14bb8cef294dac5
LabLamb <lablambworks@gmail.com>
2020-06-29 16:27:53 +0800

    Refractor lines and stations to other files

--- server.js
+++ server.js
@@ -2,58 +2,9 @@ const schedule = require('node-schedule'),
     app = require('express')(),
     http = require('http').createServer(app),
     io = require('socket.io')(http),
-    apiClient = require('./api-client')
-
-const mtrStationsAcronyms = {
-    "AEL": {
-        "HOK": "Hong Kong",
-        "KOW": "Kowloon",
-        "TSY": "Tsing Yi",
-        "AIR": "Airport",
-        "AWE": "AsiaWorld Expo"
-    },
-    "TCL": {
-        "HOK": "Hong Kong",
-        "KOW": "Kowloon",
-        "OLY": "Olympic",
-        "NAC": "Nam Cheong",
-        "LAK": "Lai King",
-        "TSY": "Tsing Yi",
-        "SUN": "Sunny Bay",
-        "TUC": "Tung Chung"
-    },
-    "WRL": {
-        "HUH": "Hung Hom",
-        "ETS": "East Tsim Sha Tsui",
-        "AUS": "Austin",
-        "NAC": "Nam Cheong",
-        "MEF": "Mei Foo",
-        "TWW": "Tsuen Wan West",
-        "KSR": "Kam Sheung Road",
-        "YUL": "Yuen Long",
-        "LOP": "Long Ping",
-        "TIS": "Tin Shui Wai",
-        "SIH": "Siu Hong",
-        "TUM": "Tuen Mun"
-    },
-    "TKL": {
-        "NOP": "North Point",
-        "QUB": "Quarry Bay",
-        "YAT": "Yau Tong",
-        "TIK": "Tiu Keng Leng",
-        "TKO": "Tseung Kwan O",
-        "LHP": "LOHAS Park",
-        "HAH": "Hang Hau",
-        "POA": "Po Lam"
-    }
-}
-
-const mtrLinesAcronyms = {
-    "AEL": "Airport Express",
-    "TCL": "Tung Chung Line",
-    "WRL": "West Rail Line",
-    "TKL": "Tseung Kwan O Line"
-}
+    apiClient = require('./api-client'),
+    mtrStationsAcronyms = require('./stations'),
+    mtrLinesAcronyms = require('./lines')
 
 app.get(`/Lines`, (req, res) => {
     res.send(mtrLinesAcronyms)
---
+++ stations.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "AEL": {
+        "HOK": "Hong Kong",
+        "KOW": "Kowloon",
+        "TSY": "Tsing Yi",
+        "AIR": "Airport",
+        "AWE": "AsiaWorld Expo"
+    },
+    "TCL": {
+        "HOK": "Hong Kong",
+        "KOW": "Kowloon",
+        "OLY": "Olympic",
+        "NAC": "Nam Cheong",
+        "LAK": "Lai King",
+        "TSY": "Tsing Yi",
+        "SUN": "Sunny Bay",
+        "TUC": "Tung Chung"
+    },
+    "WRL": {
+        "HUH": "Hung Hom",
+        "ETS": "East Tsim Sha Tsui",
+        "AUS": "Austin",
+        "NAC": "Nam Cheong",
+        "MEF": "Mei Foo",
+        "TWW": "Tsuen Wan West",
+        "KSR": "Kam Sheung Road",
+        "YUL": "Yuen Long",
+        "LOP": "Long Ping",
+        "TIS": "Tin Shui Wai",
+        "SIH": "Siu Hong",
+        "TUM": "Tuen Mun"
+    },
+    "TKL": {
+        "NOP": "North Point",
+        "QUB": "Quarry Bay",
+        "YAT": "Yau Tong",
+        "TIK": "Tiu Keng Leng",
+        "TKO": "Tseung Kwan O",
+        "LHP": "LOHAS Park",
+        "HAH": "Hang Hau",
+        "POA": "Po Lam"
+    }
+}
